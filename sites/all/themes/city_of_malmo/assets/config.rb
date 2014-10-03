e4abbccf0eaa9a5a3f49d30ced426a8004302b6d
Anton Rylov <anton.rylov@propeople.com.ua>
2014-10-03 13:25:54 +0300

    Configure config.rb for fonts and city_of_malmo.info file

--- sites/all/themes/city_of_malmo/assets/config.rb
+++ sites/all/themes/city_of_malmo/assets/config.rb
@@ -6,6 +6,7 @@ css_dir = 'assets/stylesheets'
 sass_dir = 'assets/sass'
 images_dir = 'assets/images'
 javascripts_dir = 'assets/javascripts'
+fonts_dir = "fonts/"
 relative_assets = true
 line_comments = true
 # output_style = :compressed
