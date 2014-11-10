ea5a86364c819f2d822a974dee88b48386e14331
Valera Garzha <valeriy.garzha@propeople.com.ua>
2014-11-10 15:06:47 +0200

    MAL-14 | Add school and facet by node reference

---
+++ modules/custom/mal_ctools_contact_box/plugins/content_types/contact_box.tpl.php
@@ -0,0 +1,23 @@
+<aside class="contact-us basic">
+  <h1><?php print t('Contact us'); ?></h1>
+
+  <div class="vcard">
+    <div><?php print t('Write to us'); ?></div>
+    <div><?php print empty($email['#title']) ? '' : $email['#title'] . ': '; ?><span class="email"><?php print render($email); ?></span></div>
+    <div><?php print empty($phone['#title']) ? '' : $phone['#title'] . ': '; ?><span class="tel"><?php print render($phone); ?></span></div>
+    <div><?php print empty($fax['#title']) ? '' : $fax['#title'] . ': '; ?><span class="fax"><?php print render($fax); ?></span></div>
+
+    <h3><?php print t('Post address'); ?></h3>
+    <div class="adr">
+      <div class="fn"><?php print render($address); ?></div>
+      <div class="post-office-box"><?php print render($post_office); ?></div>
+      <div><span class="postal-code"><?php print render($postal_code); ?></span> <span class="locality"><?php print render($locality); ?></span></div>
+    </div>
+
+    <h3 class="visiting-address v-0"><?php print t('Visiting Address'); ?></h3>
+    <div class="street-address"><?php print render($street_address); ?></div>
+<!--    <a href="http://www.malmo.se/karta?poi=Föreningsgatan+7A&amp;zoomlevel=4&amp;maptype=karta" class="show-on-map m-icon-location" data-map-selector=".contact-us-map.m-0" data-scroll-to=".visiting-address.v-0" data-poi="Föreningsgatan 7A" title="Föreningsgatan 7A">--><?php //print t('Show on map'); ?><!--</a>-->
+    <div><?php print render($maps_link); ?></div>
+    <div class="contact-us-map m-0"></div>
+  </div>
+</aside>
