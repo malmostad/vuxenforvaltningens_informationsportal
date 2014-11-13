77e7ff499933efaff9c1dd3a30ce686c53dcbe05
Valera Garzha <valeriy.garzha@propeople.com.ua>
2014-11-13 17:43:49 +0200

    Add blocks to front page

--- themes/city_of_malmo/templates/panels-pane.tpl.php
+++ themes/city_of_malmo/templates/panels-pane.tpl.php
@@ -25,12 +25,6 @@
     <?php print $admin_links; ?>
   <?php endif; ?>
 
-  <?php print render($title_prefix); ?>
-  <?php if ($title): ?>
-    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
   <?php if ($feeds): ?>
     <?php print $feeds; ?>
   <?php endif; ?>
