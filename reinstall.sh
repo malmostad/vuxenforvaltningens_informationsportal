0782e9e9435ae0a328db57c5d7ce31d94ba732b2
Artyom Miroshnik <artyom.miroshnik@propeople.com.ua>
2014-11-26 17:24:56 +0200

    Modify update path for MAL-66 | MAL-67

--- reinstall.sh
+++ reinstall.sh
@@ -3,22 +3,22 @@
 
 . scripts/local_variables.sh
 
-##site_folder=build_mal
-#echo "Operation : clearing files"
-#rm -rf $SITE_FOLDER
-##echo Operation : clearing drush cache
-#rm -rf ~/.drush/cache
-#echo "Operation : drush make build file"
-#cp $LOCAL_MAKE_FILE mal_working.make
-#
-#drush make mal_working.make --no-clean --prepare-install --working-copy --yes $SITE_FOLDER
-#rm mal_working.make
+#site_folder=build_mal
+echo "Operation : clearing files"
+rm -rf $SITE_FOLDER
+#echo Operation : clearing drush cache
+rm -rf ~/.drush/cache
+echo "Operation : drush make build file"
+cp $LOCAL_MAKE_FILE mal_working.make
+
+drush make mal_working.make --no-clean --prepare-install --working-copy --yes $SITE_FOLDER
+rm mal_working.make
 cd $SITE_FOLDER/profiles/mal
 
-#echo "Operation : site install"
-#drush site-install mal --account-name=admin --account-pass=admin --site-mail=admin@example.com --site-name="City of malmo" --yes --db-url="$DB_URL"
-#echo "Operation : Clear cache"
-#drush cc all
+echo "Operation : site install"
+drush site-install mal --account-name=admin --account-pass=admin --site-mail=admin@example.com --site-name="City of malmo" --yes --db-url="$DB_URL"
+echo "Operation : Clear cache"
+drush cc all
 
 . ../../../scripts/after_install.sh
 
