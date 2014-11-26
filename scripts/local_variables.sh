9eb3fe2d620d1a5969d0b186c87cc4ea6c89de57
Artyom Miroshnik <artyom.miroshnik@propeople.com.ua>
2014-11-26 13:13:40 +0200

    Remove old deprecated ansible box

--- scripts/local_variables.sh
+++ scripts/local_variables.sh
@@ -4,10 +4,10 @@ SITE_FOLDER="build_mal"
 
 export LOCAL_MAKE_FILE="local.mal.build"
 
-DB_URL="mysql://mal:mal@127.0.0.1/mal"
+DB_URL="mysql://root:root@127.0.0.1/mal"
 
-SEARCH_API_HOST="\"blk.loc\""
-SEARCH_API_PATH="\"/solr/valera_dkf\""
-SEARCH_API_PORT="\"8080\""
+SEARCH_API_HOST="\"localhost\""
+SEARCH_API_PATH="\"/solr/solr0\""
+SEARCH_API_PORT="\"8983\""
 SEARCH_API_HTTP_USER="\"\""
 SEARCH_API_HTTP_PASS="\"\""
