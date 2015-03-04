0ed9632857d29469015909c0cdd221bbaadf72e3
Temoor Gilmutdinov <temoor.gilmutdinov@propeople.com.ua>
2015-03-04 15:48:52 +0200

    MAL-223. Added usage of Drupal context to limit selectors

--- themes/city_of_malmo/javascripts/init.js
+++ themes/city_of_malmo/javascripts/init.js
@@ -16,11 +16,12 @@
         .attr('href','/')
           .css('background','url("http://assets.malmo.se/external/v4/logo-x1.png") no-repeat');
 
-      var $flex_group = $('.facetapi-facet-field-course-formname');
-      if ($('.facetapi-facet-field-course-periodsvalue-formatted').length > 0 &&
+      var $flex_group = $('.facetapi-facet-field-course-formname', context);
+      var $time_group = $('.facetapi-facet-field-course-periodsvalue-formatted', context);
+      if ($time_group.length > 0 &&
         $flex_group.find('li').length > 0) {
         $flex_group.parent().hide();
-        $flex_group.find('li').detach().appendTo('.facetapi-facet-field-course-periodsvalue-formatted');
+        $flex_group.find('li').detach().appendTo($time_group);
       }
     }
   };
